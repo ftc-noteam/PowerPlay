13716baae6dd12bdb9dc945d07640b84ff5b90ff
neil mehra <neilmehra@outlook.com>
2022-10-03 11:45:41 -0400

    woo new pathing system

--- TeamCode/src/main/java/asiankoala/testing/opmodes/KTeleOp.kt
+++ TeamCode/src/main/java/asiankoala/testing/opmodes/KTeleOp.kt
@@ -1,21 +1,15 @@
 package asiankoala.testing.opmodes
 
 import com.asiankoala.koawalib.command.KOpMode
-import com.asiankoala.koawalib.logger.Logger
 import com.asiankoala.koawalib.math.Pose
 import com.asiankoala.koawalib.math.radians
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import asiankoala.testing.Robot
-import com.acmerobotics.dashboard.config.Config
-import com.acmerobotics.roadrunner.geometry.Pose2d
-import com.acmerobotics.roadrunner.geometry.Vector2d
-import com.acmerobotics.roadrunner.path.PathBuilder
 import com.asiankoala.koawalib.command.commands.*
-import com.asiankoala.koawalib.logger.LoggerConfig
+import com.asiankoala.koawalib.path.Path
 
 @TeleOp
-@Config
-class KTeleOp : KOpMode() {
+class KTeleOp : KOpMode(photonEnabled = true) {
     private val startPose = Pose(-36.0, -60.0, 90.0.radians)
     private val robot by lazy { Robot(startPose) }
 
@@ -34,24 +28,23 @@ class KTeleOp : KOpMode() {
             )
         )
 
-        Logger.config = LoggerConfig.DASHBOARD_CONFIG
-
         driver.x.onPress(InstantCmd({ driver.rumbleBlips(3) }))
         driver.y.onPress(InstantCmd({ driver.rumble(2500) }))
 
-        val path = PathBuilder(startPose.toPose2d())
-            .splineTo(Vector2d(-24.0, -36.0), 45.0.radians)
-            .splineTo(Vector2d(-14.0, 0.0), 90.0.radians)
-            .build()
+        val path = Path(
+            startPose,
+            Pose(-24.0, -36.0, 45.0.radians),
+            Pose(-14.0, 0.0, 90.0.radians)
+        )
 
         driver.a.onPress(
             GVFCmd(
                 robot.drive,
                 path,
-                0.7,
+                0.65,
                 1.0 / 25.0,
                 4.0,
-                0.9,
+                1.0,
                 2.0
             )
         )
