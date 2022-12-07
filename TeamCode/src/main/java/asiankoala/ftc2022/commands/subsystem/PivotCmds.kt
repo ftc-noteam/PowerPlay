8f79351afab483fd74b87837bf1df6b3f65fbc2f
neil mehra <neilmehra@outlook.com>
2022-12-06 22:38:48 -0500

    why is pivotcmds not an object huh

--- TeamCode/src/main/java/asiankoala/ftc2022/commands/subsystem/PivotCmds.kt
+++ TeamCode/src/main/java/asiankoala/ftc2022/commands/subsystem/PivotCmds.kt
@@ -4,7 +4,7 @@ import asiankoala.ftc2022.subsystems.Pivot
 import asiankoala.ftc2022.subsystems.constants.PivotConstants
 import com.asiankoala.koawalib.command.commands.InstantCmd
 
-class PivotCmds {
+object PivotCmds {
     open class PivotCmd(private val pivot: Pivot, private val pos: Double) :
         InstantCmd({ pivot.setPos(pos) }, pivot)
     class PivotHomeCmd(pivot: Pivot) : PivotCmd(pivot, PivotConstants.home)
