078dd8684ab083f3bc78c7dc0ad3062f42e1bba3
neil mehra <neilmehra@outlook.com>
2022-12-25 02:54:53 -0500

    transit arm pos in intake seq

--- TeamCode/src/main/java/asiankoala/ftc2022/oryx/commands/sequence/IntakeSeq.kt
+++ TeamCode/src/main/java/asiankoala/ftc2022/oryx/commands/sequence/IntakeSeq.kt
@@ -1,12 +1,11 @@
 package asiankoala.ftc2022.oryx.commands.sequence
 
 import asiankoala.ftc2022.oryx.Oryx
-import asiankoala.ftc2022.oryx.commands.subsystem.ClawGripCmd
-import asiankoala.ftc2022.oryx.commands.subsystem.LiftHomeCmd
-import asiankoala.ftc2022.oryx.commands.subsystem.LiftReadyCmd
+import asiankoala.ftc2022.oryx.commands.subsystem.*
 import asiankoala.ftc2022.oryx.utils.State
 import com.asiankoala.koawalib.command.commands.InstantCmd
 import com.asiankoala.koawalib.command.commands.WaitUntilCmd
+import com.asiankoala.koawalib.command.group.ParallelGroup
 import com.asiankoala.koawalib.command.group.RaceGroup
 import com.asiankoala.koawalib.command.group.SequentialGroup
 import com.asiankoala.koawalib.gamepad.KButton
@@ -22,8 +21,14 @@ class IntakeSeq(
             .andPause(0.3),
         LiftReadyCmd(oryx.lift),
         InstantCmd({ oryx.state = State.READYING }),
-        ReadySeq(oryx)
-            .waitUntil(rt::isJustPressed)
+        ArmCmd(oryx.arm, 90.0)
+            .waitUntil(rt::isJustPressed),
+        ParallelGroup(
+            LiftStateCmd(oryx.lift, oryx.strategy),
+            ArmStateCmd(oryx.arm, oryx.strategy),
+            WaitUntilCmd { oryx.arm.pos > 0.0 },
+            PivotStateCmd(oryx.pivot, oryx.strategy)
+        ).waitUntil(rt::isJustPressed)
     ),
     SequentialGroup(
         WaitUntilCmd(lb::isJustPressed),
